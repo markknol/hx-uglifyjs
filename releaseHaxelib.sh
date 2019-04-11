6c9e1849c353f8c0877154df9213787712f7261a
Mark Knol <mark.knol@gmail.com>
2019-04-11 14:10:11 +0200

    Update releaseHaxelib.sh

--- releaseHaxelib.sh
+++ releaseHaxelib.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -f uglifyjs.zip
-zip -r uglifyjs.zip README.md src haxelib.json extraparams.hxml
+zip -r uglifyjs.zip README.md src haxelib.json extraParams.hxml
 haxelib submit uglifyjs.zip $HAXELIB_PWD --always
